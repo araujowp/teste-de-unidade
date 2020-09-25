ad2b98c688960fbfa8b3425dda4f3c5b395e3622
Wagner.Araujo <araujowp@gmail>
2020-09-25 09:01:43 -0300

    modulo 3

--- src/br/com/caelum/leilao/dominio/Leilao.java
+++ src/br/com/caelum/leilao/dominio/Leilao.java
@@ -8,14 +8,36 @@ public class Leilao {
 
 	private String descricao;
 	private List<Lance> lances;
-	
+
 	public Leilao(String descricao) {
 		this.descricao = descricao;
 		this.lances = new ArrayList<Lance>();
 	}
-	
+
 	public void propoe(Lance lance) {
-		lances.add(lance);
+
+		int total = qtdeLancesUsuario(lance);
+		
+		
+		if (lances.isEmpty() || podeDarLance(lance, total))
+			lances.add(lance);
+	}
+
+	private boolean podeDarLance(Lance lance, int total) {
+		return !ultimoLance().getUsuario().equals(lance.getUsuario())
+				&& total < 5;
+	}
+
+	private int qtdeLancesUsuario(Lance lance) {
+		int total = 0;
+		for (Lance l : lances) {
+			if(l.getUsuario().equals(lance.getUsuario())) total++;
+		}
+		return total;
+	}
+
+	private Lance ultimoLance() {
+		return lances.get(lances.size() - 1);
 	}
 
 	public String getDescricao() {
@@ -26,6 +48,14 @@ public class Leilao {
 		return Collections.unmodifiableList(lances);
 	}
 
-	
-	
+	public void dobraLance(Usuario usuario) {
+		Lance ultimo = null;
+        for(Lance lance : lances) {
+            if(lance.getUsuario().equals(usuario)) ultimo = lance;
+        }
+
+        propoe(new Lance(usuario, ultimo.getValor()*2));
+		
+	}
+
 }
