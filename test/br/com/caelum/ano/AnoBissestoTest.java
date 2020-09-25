29293984990961fe6fe8203e3cf358e82d0ecd21
Wagner.Araujo <araujowp@gmail>
2020-09-25 09:21:32 -0300

    ano bissexto

---
+++ test/br/com/caelum/ano/AnoBissestoTest.java
@@ -0,0 +1,27 @@
+package br.com.caelum.ano;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class AnoBissestoTest {
+
+	@Test
+	public void deveRetornoVerdadeiro() {
+		
+		AnoBissexto ano = new AnoBissexto();
+		assertEquals(true, ano.isAnoBissexto(2012));
+		assertEquals(true, ano.isAnoBissexto(2016));
+		
+	}
+	
+	@Test
+	public void deveRetornoFalso() {
+		
+		AnoBissexto ano = new AnoBissexto();
+		assertEquals(false, ano.isAnoBissexto(2015));
+		assertEquals(false, ano.isAnoBissexto(2011));
+		
+	}
+
+}
